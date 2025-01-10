1ab61a8cc54f8901fb34f070f895a12674782712
ndequeker <niels@nd-software.be>
2025-01-10 10:48:57 +0100

    Update organization config from VNG to Logius

--- js/config.js
+++ js/config.js
@@ -16,6 +16,11 @@ let respecConfig = {
                 name: "VNG Realisatie",
                 company: "VNG",
                 companyURL: "https://vng.nl/rubrieken/onderwerpen/standaarden",
+            },
+            {
+                name: "Logius",
+                company: "Logius",
+                companyURL: "https://github.com/Logius-standaarden",
             }
         ],
     authors:
