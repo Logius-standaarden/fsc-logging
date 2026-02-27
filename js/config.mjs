a12212412b1fc348b969bef2a1a6c343dbdec1c9
Alexander Green <alexander.green@logius.nl>
2026-02-27 15:05:37 +0100

    Change specStatus from 'CV' to 'VV'

--- js/config.mjs
+++ js/config.mjs
@@ -4,7 +4,7 @@ loadRespecWithConfiguration({
     useLogo: true,
     useLabel: true,
     license: "cc-by",
-    specStatus: "CV",
+    specStatus: "VV",
     specType: "ST",
     pubDomain: "fsc",
     shortName: "logging",
